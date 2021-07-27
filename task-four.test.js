d649bd3a5bf1e8fdbf22c5612e3dd05adab35cf9
Leandro <officialdevmi@gmail.com>
2021-07-27 02:34:54 -0700

    Add first test following TDD aproach

---
+++ task-four.test.js
@@ -0,0 +1,9 @@
+const capitalize = require('/task-four');
+
+test('returns first character capitalized', () => {
+    expect(capitalize('leandro')).toBe('Leandro');
+})
+
+test('It is already capitalized', () => {
+    expect(capitalize('Microverse')).toThrow('First character already capitalized');
+})
